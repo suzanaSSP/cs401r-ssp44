98e0a6da545671a12b20a543368b3f1818fd56c8
suzanaSSP <suzanasp1026@gmail.com>
2026-09-16 20:04:52 -0600

    part B1 done

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -10,3 +10,96 @@
 # here will cost you points in Lab 2.
 
 # TODO: implement the three resources above.
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  bucket_name = "${var.project}-${var.environment}-data-${data.aws_caller_identity.current.account_id}"
+  bucket_arn  = "arn:aws:s3:::${local.bucket_name}"
+}
+
+resource "aws_iam_role" "ml_engineer" {
+  name = "${var.project}-${var.environment}-MLEngineer"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "sagemaker.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project}-${var.environment}-MLEngineer"
+  }
+}
+
+resource "aws_iam_policy" "ml_engineer" {
+  name        = "${var.project}-${var.environment}-MLEngineer-policy"
+  description = "Policy for MLEngineer with restricted S3 write access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+ 
+      {
+        Effect = "Allow"
+        Action = [
+          "sagemaker:CreateTrainingJob",
+          "sagemaker:DescribeTrainingJob",
+          "sagemaker:StopTrainingJob",
+          "sagemaker:ListTrainingJobs",
+          "sagemaker:CreateModel",
+          "sagemaker:DescribeModel",
+          "sagemaker:DeleteModel",
+          "sagemaker:CreateEndpointConfig",
+          "sagemaker:CreateEndpoint",
+          "sagemaker:DescribeEndpoint",
+          "sagemaker:InvokeEndpoint",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Resource = local.bucket_arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = "${local.bucket_arn}/*"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "${local.bucket_arn}/processed/*",
+          "${local.bucket_arn}/features/*",
+          "${local.bucket_arn}/artifacts/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ml_engineer" {
+  role       = aws_iam_role.ml_engineer.name
+  policy_arn = aws_iam_policy.ml_engineer.arn
+}
--- infrastructure/modules/sagemaker/main.tf
+++ infrastructure/modules/sagemaker/main.tf
@@ -18,3 +18,39 @@
 # module call in environments/dev/main.tf.
 
 # TODO: implement the two resources above.
+
+resource "aws_sagemaker_domain" "this" {
+  domain_name = "${var.project}-${var.environment}-domain"
+  auth_mode   = "IAM"
+  vpc_id      = var.vpc_id
+  subnet_ids  = var.subnet_ids
+
+  default_user_settings {
+    execution_role  = var.execution_role_arn
+    security_groups = var.security_group_ids
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-domain"
+  }
+
+  retention_policy {
+    home_efs_file_system = "Delete"
+  }
+  tags = {
+    Name = "${var.project}-${var.environment}-domain"
+  }
+}
+
+resource "aws_sagemaker_user_profile" "this" {
+  domain_id         = aws_sagemaker_domain.this.id
+  user_profile_name = "${var.project}-${var.environment}-user"
+
+  user_settings {
+    execution_role = var.execution_role_arn
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-user"
+  }
+}
